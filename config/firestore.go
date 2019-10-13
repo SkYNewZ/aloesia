4b86280e8ea2277ea5711d8bc8bb37c24a69f4e8
Quentin Lemaire <quentin@lemairepro.fr>
2019-10-13 02:32:05 +0200

    Configure Firestore SDK

--- config/firestore.go
+++ config/firestore.go
@@ -1,15 +1,31 @@
 package config
 
-// TODO: Implement this file
+import (
+	"context"
+	"log"
+	"os"
 
-var firestore int
+	"cloud.google.com/go/firestore"
+)
+
+var firestoreClient *firestore.Client
 
 // InitFirestoreDatabase init main firestore client
 func InitFirestoreDatabase() {
-	firestore = 1
+	var err error
+	firestoreClient, err = firestore.NewClient(context.Background(), os.Getenv("GOOGLE_PROJECT_ID"))
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
 }
 
 // Firestore Getter for firestore database
-func Firestore() int {
-	return firestore
+func Firestore() *firestore.Client {
+	return firestoreClient
+}
+
+// CloseConnection clone connection with Firebase
+func CloseConnection() {
+	// Close client when done.
+	defer firestoreClient.Close()
 }
