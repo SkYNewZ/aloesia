5c4954ac2849ef9e5842f39bb8247501777e01c4
Quentin Lemaire <quentin@lemairepro.fr>
2019-10-13 02:44:23 +0200

    Improve tests: use init() function for optimization

--- main_test.go
+++ main_test.go
@@ -4,19 +4,25 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
+var router *mux.Router
+var rr *httptest.ResponseRecorder
+
+func init() {
+	router = InitializeRouter()
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr = httptest.NewRecorder()
+}
+
 func TestHealthCheckHandler(t *testing.T) {
-	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
-	// pass 'nil' as the third parameter.
 	req, err := http.NewRequest("GET", "/api/health", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
-	InitializeRouter().ServeHTTP(rr, req)
+	router.ServeHTTP(rr, req)
 
 	// Check the status code is what we expect.
 	if status := rr.Code; status != http.StatusOK {
