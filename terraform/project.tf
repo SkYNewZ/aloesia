fe39463d0fcc7336f0f2b05c4395e113a4bcbbfe
Quentin Lemaire <quentin@lemairepro.fr>
2020-01-09 19:03:49 +0100

    Add billing_account

--- terraform/project.tf
+++ terraform/project.tf
@@ -1,6 +1,7 @@
 module "generic-project" {
-  source              = "github.com/skynewz/generic-gcp-project"
-  folder_name         = "pro"
-  project_id          = var.project_id
-  project_name        = "Aloesia"
+  source          = "github.com/skynewz/generic-gcp-project"
+  folder_name     = "pro"
+  project_id      = var.project_id
+  project_name    = "Aloesia"
+  billing_account = var.billing_account
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,6 +3,11 @@ variable "project_id" {
   description = "Project it to set to"
 }
 
+variable "billing_account" {
+  type    = string
+  default = "019C2D-785389-9E0921"
+}
+
 variable "region" {
   type        = string
   description = "Where to deploy resources"
@@ -10,6 +15,6 @@ variable "region" {
 }
 
 variable "send_email_environment_variables" {
-  type = map
+  type        = map
   description = "Map of key/values HCL pairs to set into mail Cloud Function"
 }
